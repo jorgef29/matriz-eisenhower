9688fa9e8f9c655f88f0e1443a8813cd857fe173
jorgef29 <jorgefigueredo2001@gmail.com>
2024-08-17 23:08:52 -0400

    chore: Update deployment directory to 'dist' in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/matriz-eisenhower/', // Reemplaza 'nombre-repositorio' con el nombre de tu repositorio
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist', // Asegúrate de que el directorio de salida sea 'dist'
+  },
+});
